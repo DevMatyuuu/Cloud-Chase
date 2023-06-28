50e50e82bda2d0560758ed03d7e8a4b1e3999cd3
R0su <2019-104574@rtu.edu.ph>
2023-06-28 11:27:44 +0800

    fix

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,11 +1,22 @@
 module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+
   {
     name: 'cors',
+    resolve: 'koa-cors',
     config: {
       origin: ['https://cloud-chase.vercel.app', 'http://cloud-chase.vercel.app'],
     },
   },
-];
+]
 
 
 
