0c5d3052673b74de2f63945ef2f774f3386d8996
R0su <2019-104574@rtu.edu.ph>
2023-06-28 11:52:33 +0800

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -3,26 +3,20 @@ const path = require('path');
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
-  const connections = {
-    default: {
-      connector: 'strapi-hook-bookshelf',
-      settings: {
-        client,
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'), // Path to the SQLite database file
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  };
-
-  return {
+  module.exports = ({ env }) => ({
     defaultConnection: 'default',
-    connections,
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    connections: {
+      default: {
+        connector: 'strapi-hook-bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'), // Path to the SQLite database file
+        },
+        options: {
+          useNullAsDefault: true,
+        },
+      },
     },
-  };
-};
+  })
+}
+
