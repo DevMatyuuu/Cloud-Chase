8dbb2629b07f2003210792ef3488e80b7b51ae60
R0su <2019-104574@rtu.edu.ph>
2023-06-28 12:36:10 +0800

    Update admin.js

--- api/config/admin.js
+++ api/config/admin.js
@@ -1,23 +1,32 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
     // Other plugin settings...
     async register(ctx) {
       const { email, password } = ctx.request.body;
 
       try {
+        // Check if an admin user already exists in the database
+        const adminUsers = await strapi.plugins['users-permissions'].services.user.fetchAll({
+          role: 1, // Role ID for admin (adjust according to your role setup)
+        });
+
+        if (adminUsers.length > 0) {
+          // Admin user already exists, do not perform registration
+          return ctx.badRequest('Admin user already registered.');
+        }
+
         // Create the admin user in the database
         const adminUser = await strapi.plugins['users-permissions'].services.user.add({
           email,
           password,
-          role: 1, // Set the role ID for admin (adjust according to your role setup)
+          role: 1, // Role ID for admin (adjust according to your role setup)
         });
 
         // Return the registered admin user as the response
         return adminUser;
       } catch (error) {
         // Handle any errors that occur during registration
-        return error;
+        return ctx.badRequest('Failed to register admin user.');
       }
     },
   },
