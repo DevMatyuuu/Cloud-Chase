9438c8b7497c7282bcfac3ece45c2b3dc08d0ed9
R0su <2019-104574@rtu.edu.ph>
2023-06-28 10:42:32 +0800

    fix

--- api/config/database.js
+++ api/config/database.js
@@ -1,18 +1,28 @@
 const path = require('path');
 
-  module.exports = ({ env }) => ({
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'strapi-hook-bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'), // Path to the SQLite database file
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+module.exports = ({ env }) => {
+  const client = 'sqlite';
+
+  const connections = {
+    default: {
+      connector: 'strapi-hook-bookshelf',
+      settings: {
+        client,
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'), // Path to the SQLite database file
+      },
+      options: {
+        useNullAsDefault: true,
       },
     },
-  });
+  };
 
+  return {
+    defaultConnection: 'default',
+    connections,
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -2,7 +2,7 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      origin: 'https://cloud-chase.vercel.app, http://cloud-chase.vercel.app'
+      origin: ''
     },
   },
 };
