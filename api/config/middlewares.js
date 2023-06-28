329b437df533328cc17cf30611150e13ecd679f9
R0su <2019-104574@rtu.edu.ph>
2023-06-28 11:11:24 +0800

    Update middlewares.js

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -2,7 +2,7 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      origin: 'https://cloud-chase-web.onrender.com, http://cloud-chase-web.onrender.com'
+      origin: 'https://cloud-chase.vercel.app, http://cloud-chase.vercel.app'
     },
   },
 };
