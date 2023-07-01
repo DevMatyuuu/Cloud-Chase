77d03f19cfff2f9a2176655899e9418c48005359
R0su <2019-104574@rtu.edu.ph>
2023-07-01 15:43:56 +0800

    Update Cart.jsx

--- cloud-chase/src/components/Cart.jsx
+++ cloud-chase/src/components/Cart.jsx
@@ -13,7 +13,7 @@ function Cart() {
 
   return (
     <>
-      <div className={`w-full md:h-[650px] sl:h-[650px] h-auto ${isCartEmpty ? 'h-auto overflow-y-hidden' : 'h-auto'} max-h-full px-4 shadow-left-2xl rounded-xl`}>
+      <div className={`w-full md:h-[650px] sl:h-[650px] h-[700px] ${isCartEmpty ? 'h-[700px] overflow-y-hidden' : 'h-auto'} max-h-full px-4 shadow-left-2xl rounded-xl`}>
         <div className='flex-row'>
           <SlClose
             onClick={() => setIsOpen(false)}
