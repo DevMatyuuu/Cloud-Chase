9602ce43de860f9bf8b52b5e9a126f301005eccd
R0su <2019-104574@rtu.edu.ph>
2023-07-08 22:50:29 +0800

    Update Search.jsx

--- cloud-chase/src/pages/Search.jsx
+++ cloud-chase/src/pages/Search.jsx
@@ -15,27 +15,27 @@ function Search() {
 
   return (
     <div>
-      <div className="container mx-auto md:mx-auto mt-10 md:mt-10 md:pb-[104px] pb-[117px]">
-        <span className="uppercase text-white font-bold md:text-3xl text-md md:mt-0 md:ml-3 ml-9">
+      <div className="container mx-auto md:mx-auto mt-10 md:mt-10 md:pb-[104px] pb-[180px]">
+        <span className="uppercase text-white font-bold md:text-3xl sl:text-2xl sm:text-xl text-md md:mt-0 md:ml-3 ml-9">
           Result for: "{searchTerm}"
         </span>
-        <div className=''>
+        <div className='mx-auto'>
           {isLoading ? (
             // Show loading state
-            <div className="text-white mx-auto md:w-36 md:font-bold md:text-2xl md:mt-8 ml-32 mt-20 w-32 md:ml-[650px] md:pb-36"><Lottie animationData={loading}/></div>
+            <div className="text-white mx-auto mt-20 md:pb-[145px] sl:pb-[60px] sm:pb-[50px] pb-14"><Lottie animationData={loading} className='mx-auto h-32'/></div>
           ) : data && data.length > 0 ? (
             // Show data if available
             data.map((product) => (
-              <div className="grad bg-gradient-to-b from-gray-900 to-gray-600 w-full md:h-[320px] md:max-w-[300px] max-w-[180px] sm:h-[290px] sm:max-w-[200px] sl:h-[240px] h-[250px] rounded-[8px] overflow-hidden relative group justify-center shadow-xl sm:mt-5 ml-9 md:ml-4 md:mt-8" key={product.id}>
+              <div className="grad bg-gradient-to-b from-gray-900 to-gray-600 w-full md:h-[320px] md:max-w-[300px] max-w-[180px] sm:h-[300px] sm:max-w-[200px] sl:h-[260px] sl:max-w-[240px] h-[250px] rounded-[8px] overflow-hidden relative group justify-center shadow-xl sm:mt-5 ml-9 md:ml-4 md:mt-8 mt-5" key={product.id}>
                 <Product product={product} />
               </div>
             ))
           ) : (
             // Show no results message
-               <div className='w-full md:h-[355px] sm:h-60 h-60 text-center'>
-                <Lottie animationData={noResult} className='md:h-80 sl:h-60 sm:h-60 h-40'/>
+               <div className='w-full md:h-[355px] sm:h-60 h-[200px] text-center mt-16 md:mt-0 sl:mt-5 sm:mt-5 sl:pb-[249px]'>
+                <Lottie animationData={noResult} className='md:h-80 sl:h-80 sm:h-60 h-40'/>
                 <div>
-                  <span className='md:text-xl text-white'>No Result Found</span>
+                  <span className='md:text-xl sl:text-xl sm:text-lg text-white'>No Result Found</span>
                 </div>
                </div>
           )}
