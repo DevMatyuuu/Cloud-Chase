de7d4dcd3ae4cc75afc7bb08ac7439dca9cf0fcb
R0su <2019-104574@rtu.edu.ph>
2023-09-25 23:29:25 +0800

    added email sent modal

--- cloud-chase/src/pages/ContactUs.jsx
+++ cloud-chase/src/pages/ContactUs.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Button } from '@material-tailwind/react'
+import Sent from '../assets/sent.json'
+import Modal from 'react-modal'
+import Lottie from 'lottie-react'
 
 function ContactUs() {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setModalOpen(true);
+        setTimeout(() => {
+          setModalOpen(false);
+        }, 2000);
+      };
+
   return (
     <section className='text-white'>
-        <div className="py-8 lg:py-16 px-6 mx-auto md:max-w-[1000px] sl:max-w-[1000px] sm:max-w-[600px] md:mt-10 sl:mt-5 sm:mt-5 sm:mb-5 mt-5 mb-5 sl:mb-5 md:mb-10 rounded-xl bg-gradient-to-b from-gray-900 to-gray-600">
+        <div className="py-8 lg:py-16 z-0 px-6 mx-auto md:max-w-[1000px] sl:max-w-[1000px] sm:max-w-[600px] md:mt-10 sl:mt-5 sm:mt-5 sm:mb-5 mt-5 mb-5 sl:mb-5 md:mb-10 rounded-xl bg-gradient-to-b from-gray-900 to-gray-600">
             <h2 className="mb-7 sl:text-4xl md:text-4xl sm:text-2xl tracking-tight font-poppins font-bold text-center text-white uppercase">Contact Us</h2>
             <p className="mb-8 sm:mb-6 lg:mb-16 font-poppins text-center text-white sm:text-md">Got a technical issue? Want to send feedback about our service? Need assistance about our product? Let us know.</p>
             <form action="#" className="space-y-8">
@@ -20,9 +32,18 @@ function ContactUs() {
                     <label for="message" className="block mb-2 text-sm font-medium text-white dark:text-gray-400">Your message</label>
                     <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
                 </div>
-                <Button type="submit" className="py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-white sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</Button>
+                <Button onClick={handleOpenModal} type="submit" className="py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-white sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</Button>
             </form>
         </div>
+        <Modal
+        isOpen={modalOpen}
+        onRequestClose={() => setModalOpen(false)}
+        contentLabel="Modal"
+        className='modal z-50 fixed inset-0 flex items-center justify-center bg-transparent'
+        overlayClassName="modal-overlay fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center" // Tailwind CSS classes for the modal overlay
+      >
+            <Lottie animationData={Sent} className='max-w-[400px] max-h-[400px]'/>
+        </Modal>
   </section>
   )
 }
